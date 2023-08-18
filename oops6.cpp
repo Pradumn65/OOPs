d217dce5ebf80251e24bfc10d12a8f6143f341ba
Pradumn65 <114013210+Pradumn65@users.noreply.github.com>
2023-08-18 13:22:22 +0530

    Add files via upload

---
+++ oops6.cpp
@@ -0,0 +1,25 @@
+// parameterised contructor
+#include<iostream>
+using namespace std;
+
+class car{
+    public:
+    string brand;
+    int car_no;
+ 
+    car(string brandname,int carno){
+        brand=brandname;
+        car_no= carno;
+    }
+
+
+};
+  
+int main()
+{
+    car scorpio("Mahindra",1000);
+    cout<<scorpio.brand<<endl;
+    cout<<scorpio.car_no<<endl;
+
+    return 0;
+}
